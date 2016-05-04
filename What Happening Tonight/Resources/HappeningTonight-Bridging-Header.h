48ed78ce6ea17486582dce992444c8b1be658c0d
dannymlevine <dannymlevine@aol.com>
2016-05-04 14:28:13 -0700

    Got all my files ported over

--- What Happening Tonight/Resources/HappeningTonight-Bridging-Header.h
+++ What Happening Tonight/Resources/HappeningTonight-Bridging-Header.h
@@ -7,8 +7,6 @@
 #ifndef ParseStarterProject_Bridging_Header_h
 #define ParseStarterProject_Bridging_Header_h
 
-
-#import <FactualSDK/FactualAPI.h>
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 
